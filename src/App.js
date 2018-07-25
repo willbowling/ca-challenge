019fe74e73d55aae5ba320b111b0f67e9e3e473e
Will Bowling <will.bowling@weather.com>
2018-07-25 10:10:22 -0400

    some small cleanup with css

--- src/App.js
+++ src/App.js
@@ -7,6 +7,8 @@ import './App.css';
 // 3.  When the name and phone number is submitted, the time slot selected should change to red, indicating the time slot is no longer available.
 // 4.   If the red time slot is clicked on again, the modal will pop up with the name and phone number for that appointment pre - populated.Users will be able to edit the name and phone number to change the user for the appointment.
 
+// TODO: Update With Modal - coming soon.
+
 class Challenge extends Component {
 
   render() {
@@ -19,25 +21,26 @@ class Challenge extends Component {
         <header className="App-header">
           <h1 className="App-title">Welcome to Cox Automotive Code Challenge</h1>
         </header>
-        <form>
-          <label htmlFor="time">
-            Please select desired appointment time
-          </label>
-          <hr />
-          <select name="time" aria-label="time">
-            <option value="9:00am">9:00am</option>
-            <option value="10:00am">10:00am</option>
-            <option value="11:00am">11:00am</option>
-            <option value="12:00pm">12:00pm</option>
-            <option value="1:00pm">1:00pm</option>
-            <option value="2:00pm">2:00pm</option>
-            <option value="3:00pm">3:00pm</option>
-            <option value="4:00pm">4:00pm</option>
-            <option value="5:00pm">5:00pm</option>
-          </select>
-          <button label="Confirm Time"> Confirm Time </button>
-        </form>
-        <Moment time={timeToFormat} />
+        <main id="main" role="main">
+          <form>
+            <label htmlFor="time">
+              Please select desired appointment time
+            </label>
+            <select name="time" aria-label="time">
+              <option value="9:00am">9:00am</option>
+              <option value="10:00am">10:00am</option>
+              <option value="11:00am">11:00am</option>
+              <option value="12:00pm">12:00pm</option>
+              <option value="1:00pm">1:00pm</option>
+              <option value="2:00pm">2:00pm</option>
+              <option value="3:00pm">3:00pm</option>
+              <option value="4:00pm">4:00pm</option>
+              <option value="5:00pm">5:00pm</option>
+            </select>
+            <button label="Confirm Time"> Confirm Time </button>
+          </form>
+          <Moment time={timeToFormat} />
+        </main>
       </section>
     );
   }
